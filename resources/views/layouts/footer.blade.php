a8ef0969a02f3181a4ea322f533db6704a869195
Gantabya Prottoy <gantabyakprottoy96@gmail.com>
2025-01-19 08:11:37 +0600

    video record included

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+
+<footer class="bg-white p-4 text-center">
+    <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+    <span class="block text-sm text-slate-500 text-center">Footer Area</span>
+</footer>
+</body>
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header style="border-bottom:1px solid #dddddd">
+        <h2>Header Area</h2>
+    </header>
+</body>
+</html>
